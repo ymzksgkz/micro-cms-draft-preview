8b05b6ec14b286cc76af869c30afa84853e3f533
Shigekazu Yamazaki <83740740+shigekazu-branu@users.noreply.github.com>
2022-10-07 16:40:22 +0900

    記事取得

--- pages/draft.vue
+++ pages/draft.vue
@@ -14,7 +14,20 @@ export default {
       data: {}
     }
   }, async created() {
-    console.log('aa')
+    // https://blog.microcms.io/nuxt-jamstack-preview/
+    const query = this.$route.query
+    if (query.id === undefined || query.draftKey === undefined) {
+      return
+    }
+    const { data } = await axios.get(
+      `https://cms-practice.microcms.io/api/v1/news/${query.id}?draftKey=${query.draftKey}`,
+      {
+        // TODO 環境変数にしたほうがよいけど一旦いい。
+        headers: { 'X-MICROCMS-API-KEY': 'a85dd8764e99460e92d2bdf73906732c1a56' }
+      }
+    )
+    this.data = data
+
   }
 }
 </script>
